b40389c39e3b9b5e2c2331a0e621b7d9dac82c17
Paul George Savluc <102178068+PaulsGitHubs@users.noreply.github.com>
2025-12-04 17:38:37 -0500

    Create ai_integegrations.py

---
+++ ai_integration/ai_integegrations.py
@@ -0,0 +1,3 @@
+Allow people to ask ai to control the cesium environement and be able to inject code within the IDE for the user.
+
+                                                                                                   This is gooing to look very cool when we link to sensors and other things 
