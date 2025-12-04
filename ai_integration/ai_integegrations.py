6b6ebe95497741c588f609ca14131d23472a771f
Yash Patel <yashnpatel3@gmail.com>
2025-12-04 17:19:59 -0600

    Update ai_integegrations.py

--- ai_integration/ai_integegrations.py
+++ ai_integration/ai_integegrations.py
@@ -1,3 +1,2 @@
 Allow people to ask ai to control the cesium environement and be able to inject code within the IDE for the user.
-
-                                                                                                   This is gooing to look very cool when we link to sensors and other things 
+This is gooing to look very cool when we link to sensors and other things 
