c959e796071b1a3de4bdcc322cc5bd2089c1841d
Paul George Savluc <102178068+PaulsGitHubs@users.noreply.github.com>
2025-12-04 17:43:14 -0500

    Create Server_Host.py

---
+++ Server_Host.py
@@ -0,0 +1,4 @@
+Write a flask server that can run the entire front end for the digital twin (intelligently), it understand the paths (/physics, /sensors, and all related files within) and makes sure that the logic can serve the digital_twin.html (which brings all the code together).
+
+We are just organizing it within different paths so people can contribute better.
+
