42865c501b03851e93e5231e22fd0451ffe79293
Paul George Savluc <102178068+PaulsGitHubs@users.noreply.github.com>
2025-12-04 17:40:30 -0500

    Create physics.js

---
+++ physics/physics.js
@@ -0,0 +1,6 @@
+Physics can be written in both JS or Python.
+  An example of physics is controling different objects based on weight, size, environment, etc.
+
+  Things like collision detection, water fluidity, etc should be worked on here
+
+Play control should also be worked on here. It is a good idea to make sure that the player controls are very user friendly and realistic. 
