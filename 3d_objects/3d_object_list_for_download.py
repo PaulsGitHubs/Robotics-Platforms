016415e76a63c5fb0e2e90ed42bc173dffb80686
Paul George Savluc <102178068+PaulsGitHubs@users.noreply.github.com>
2025-12-04 20:11:57 -0500

    Update 3d_object_list_for_download.py

--- 3d_objects/3d_object_list_for_download.py
+++ 3d_objects/3d_object_list_for_download.py
@@ -1,1 +1,12 @@
 Allow users to upload their 3d objects and save the paths either on the internet or on this github. We need later to sync to our server. 
+
+or a list that we can write into a script to make it downloadable from link.
+example: [https://www.roboticsplatforms.com/digital_twin/objectfile.glb, object_name, https://www.roboticsplatforms.com/digital_twin/objectfile.glb, https://www.roboticsplatforms.com/digital_twin/objectfile.glb, object_name]
+
+when developing locally on your own computer with flask and js you paths are going to look like: [www.localhost/5000/3d_object, object_name].
+
+pseudo code:
+@app.route
+upload2dobject(3dobject{x}):
+allow user to upload the object through a flask route that recieves it from the client and saves locally on your computer. then we deploy on the server and allow user to upload their 3d objects to it.
+
